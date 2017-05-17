fc0a84be82591d3edbb02ff1ea64c0cde3412b72
305-2 <saresalehi@yaoo.com>
2017-05-17 19:18:36 +0430

    some changes in wating_q table/wednesday

--- routes/api.js
+++ routes/api.js
@@ -95,6 +95,7 @@ router.post('/scans/:pid', upload.array('file'), apiResponse('Document','saveSca
 router.delete('/document/:did', apiResponse('Document', 'delete', false, ['params.did']));
 //WatingSaf API
 router.put('/waitingSaf',apiResponse('WaitingSaf','addToSaf',false,['body']));
+router.get('/waitingSaf', apiResponse('waitingSaf', 'select', false));
 
 
 module.exports = router;
