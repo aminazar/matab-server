59b68f9442750e43654ab6ebc0b9a6fbc500ccad
305-2 <saresalehi@yaoo.com>
2017-05-17 17:01:59 +0430

    some changes in wating_q table

--- sql/waitingSaf/addNewPatientToWating_q.sql
+++ sql/waitingSaf/addNewPatientToWating_q.sql
@@ -1,1 +1,9 @@
-insert into waiting_q(did,pid,page_num,note_num,priority) values (${did},${pid},${page_num},${note_num},(select coalesce((select max(priority) as pr from waiting_q where did=${did}),0)+1));
+insert into waiting_q(did,pid,page_num,note_num,priority)
+values
+(
+  ${did},
+  ${pid},
+  ${page_num},
+  ${note_num},
+  (select coalesce((select max(priority) as pr from waiting_q where did=${did}),0)+1)
+);
--- sql/waitingSaf/create.sql
+++ sql/waitingSaf/create.sql
@@ -1,10 +1,10 @@
 create table waiting_q(
     wsid serial not null primary key,
-	uid integer not null,
+	did integer not null,
 	pid integer not null,
 	page_num integer not null,
 	note_num integer not null,
 	priority numeric not null,
 	visit_date date not null default current_date,
-	unique(uid,pid,visit_date)
+	unique(did,pid,visit_date)
 )
