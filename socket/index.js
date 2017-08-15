066e42ffd870e635a006fab973400f28d4ed2289
arhariri <ali.71hariri@gmail.com>
2017-08-15 18:29:34 +0430

    Adding load balancing ability for socket.io Store, fetch and delete user namespace for socket.io

--- socket/index.js
+++ socket/index.js
@@ -1,64 +1,108 @@
 'use strict';
 const sessionConfig = require('../session');
 var socketIOSession = require("socket.io.session");
-const  socketRoutes = require("./socketRoutes");
+const socketRoutes = require("./socketRoutes");
+const passportSocketIO = require('passport.socketio');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const redis = require('../redis');
 
+const NEW_MESSAGE = 'NEW_MESSAGE';
 const NEW_VISIT_CMD = 'newVisit';
 const DISMISS_CMD = 'dismiss';
 const REFER_VISIT_CMD = 'referVisit';
 let io;
 let setup = http => {
 
-    this.io = require('socket.io')(http);
+  io = require('socket.io')(http);
+  io.use(passportSocketIO.authorize({
+    key: 'connect.sid',
+    secret: 'HosKhedIDA',
+    store: sessionConfig.session_config.store,
+    passport: passport,
+    cookieParser: cookieParser,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  }));
+  io.adapter(redis.redis_socket({host: 'localhost', port: 6379}));
+  io.set('transports', ['websocket']);
 
-    this.io.set('transports', ['websocket']);
+  let socketSession = socketIOSession(sessionConfig.session_config);
 
-    let socketSession = socketIOSession(sessionConfig.session_config);
+  //parse the "/" namespace
+  io.use(socketSession.parser);
 
-    //parse the "/" namespace
-    this.io.use(socketSession.parser);
-
-     socketRoutes.setup(this.io, socketSession.parser);
-
+  socketRoutes.setup(io, socketSession.parser);
 };
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('Successful connection to socket.io');
+  accept();
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error)
+    accept(new Error(message));
+
+  console.log('Failed connection  to socket.io', message);
+  accept(null, false);
+}
 
 
-let sendNewVisitMessageToAllClients = (data) =>{
-    return promise(NEW_VISIT_CMD, data[0] , socketRoutes.getPatientIO())
+let sendNewVisitMessageToAllClients = (data) => {
+  return promise(NEW_VISIT_CMD, data[0], socketRoutes.getPatientIO())
 };
 
-let sendDismissMessageToAllClients = (data) =>{
-    return promise(DISMISS_CMD , data , socketRoutes.getPatientIO());
+let sendDismissMessageToAllClients = (data) => {
+  return promise(DISMISS_CMD, data, socketRoutes.getPatientIO());
 };
 
-let sendReferMessageToAllClients = (data) =>{
-    console.log('=======>' , data);
+let sendReferMessageToAllClients = (data) => {
+  console.log('=======>', data);
 
-    return promise(REFER_VISIT_CMD, data , socketRoutes.getPatientIO());
+  return promise(REFER_VISIT_CMD, data, socketRoutes.getPatientIO());
 };
 
+let sendMessage = (data, namespace) => {
+  return new Promise((resolve, reject) => {
+    socketRoutes.isNamespaceExist(namespace)
+      .then(ns => {
+        if(ns)
+          promise(NEW_MESSAGE, data, ns)
+            .then(res => resolve(res))
+            .catch(err => {
+              console.log('Error when calling promise function: ', err);
+              reject(err);
+            });
+        else
+          reject('No namespace found');
+      });
+  });
+};
 
-let promise =  (cmd , data , io) => {
+let promise = (cmd, data, io) => {
 
-    return new Promise((resolve , reject) => {
-        setTimeout(() => {
-            let message = {
-                cmd: cmd,
-                msg:data
-            };
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let message = {
+        cmd: cmd,
+        msg: data
+      };
 
-            io.emit('ans', message);
-            resolve(data);
+      io.emit('ans', message);
+      resolve(data);
 
-        } ,0)
-    })
+    }, 0)
+  })
 
 };
 
 module.exports = {
-    setup,
-    sendNewVisitMessageToAllClients,
-    sendDismissMessageToAllClients,
-    sendReferMessageToAllClients
-}
+  setup,
+  sendNewVisitMessageToAllClients,
+  sendDismissMessageToAllClients,
+  sendReferMessageToAllClients,
+  storeNamespace: socketRoutes.saveNamespace,
+  getNamespace: socketRoutes.isNamespaceExist,
+  deleteNamespace: socketRoutes.deleteNamespace,
+};
--- socket/socketRoutes.js
+++ socket/socketRoutes.js
@@ -9,56 +9,104 @@
  *
  */
 
+const redis = require('../redis');
+
 let userIO;
 let patientIO;
+let _io;
 
 let setup = (io, socketSessionParser) => {
+  _io = io;
 
+  userIO = io.of('/user');
+  let userConnection = userIO.on('connection', socket => {
 
-    userIO = io.of('/user');
-    let userConnection = userIO.on('connection', socket => {
+    console.warn('===> user-socket-connected ', socket.session.passport.user.username);
 
-        console.warn('===> user-socket-connected ', socket.session.passport.user.username);
-
-        socket.on('req', (message) => {
-            console.log('user socket message ===> ', message);
-        });
-
+    socket.on('req', (message) => {
+      console.log('user socket message ===> ', message);
     });
-    userConnection.use(socketSessionParser);
 
-    patientIO = io.of('/patient');
-    let patientConnection = patientIO.on('connection', socket => {
+  });
+  userConnection.use(socketSessionParser);
+
+  patientIO = io.of('/patient');
+  let patientConnection = patientIO.on('connection', socket => {
 
 
-        console.warn('===> patient-socket-connected ', socket.session.passport.user.username);
+    console.warn('===> patient-socket-connected ', socket.session.passport.user.username);
 
-        socket.on('req', (message) => {
-            console.log('patient socket message ===> ', message);
+    socket.on('req', (message) => {
+      console.log('patient socket message ===> ', message);
 
-            socket.broadcast.emit('ans', message)
-        });
-
+      socket.broadcast.emit('ans', message)
     });
-    patientConnection.use(socketSessionParser);
 
+  });
+  patientConnection.use(socketSessionParser);
 
+  getAllNamespaces()
+    .then(res => {
+      for(let ns of res){
+        _io.of(ns).on('connection', socket => {
+          //Code to execute after any clients connected to specific namespace
+        });
+      }
+    })
+    .catch(err => console.log('Error: when fetch all namespaces. ', err));
 };
 
-let getUserIO = () =>{
+let getUserIO = () => {
 
-    return userIO;
+  return userIO;
 };
 
-let getPatientIO = () =>{
+let getPatientIO = () => {
 
-    return patientIO;
+  return patientIO;
 };
 
+let saveNamespace = (namespace) => {
+  return new Promise((resolve, reject) => {
+    redis.redis_client.saddAsync('namespaces', namespace)
+      .then(res => {
+        _io.of(namespace).on('connection', socket => {
+          //Code to execute after clients connected to specific namespace
+        });
+        resolve();
+      })
+      .catch(err => {
+        reject('Cannot store namespace on redis');
+      });
+  });
+};
+
+let isNamespaceExist = (namespace) => {
+  return new Promise((resolve, reject) => {
+    redis.redis_client.sismemberAsync('namespaces', namespace)
+      .then(res => {
+        if(res)
+          resolve(_io.of(namespace));
+        else
+          reject(null);
+      })
+      .catch(err => reject(err));
+  });
+};
+
+let getAllNamespaces = () => {
+  return redis.redis_client.smembersAsync('namespaces');
+};
+
+let deleteNamespace = (namespace) => {
+  return redis.redis_client.sremAsync('namespace', namespace);
+};
 
 module.exports = {
-    setup,
-    getUserIO,
-    getPatientIO
-
+  setup,
+  getUserIO,
+  getPatientIO,
+  saveNamespace,
+  isNamespaceExist,
+  deleteNamespace,
 };
